b51e559908e2b768c8ad836f51b122d7035487d6
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-22 22:56:33 +0100

    style todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,13 @@
 import { useDispatch } from "react-redux";
 import { todos } from "../reducers/todos";
 import DeleteIcon from "@mui/icons-material/Delete";
+import './List.css';
 
 const Todo = ({ todo }) => {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <div className="todoContainer">
       <span>{todo.text}</span>
       <DeleteIcon onClick={() => dispatch(todos.actions.removeTodo(todo))} />
     </div>
